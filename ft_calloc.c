b9f4a08e00bfb1e64d5ad56a5e0c431ab4288915
Wei Yee Shee <wshee@u81z05s05.42kl.edu.my>
2024-11-09 21:55:23 +0800

    added calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,34 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wshee <wshee@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/07 10:23:08 by wshee             #+#    #+#             */
-/*   Updated: 2024/11/09 21:54:19 by wshee            ###   ########.fr       */
+/*   Created: 2024/11/09 20:55:45 by wshee             #+#    #+#             */
+/*   Updated: 2024/11/09 21:55:07 by wshee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	*ft_memset(void *s, int x, unsigned int n)
-{
-	unsigned char	*p;
-
-	p = (unsigned char *)s;
-	while (n--)
-	{
-		*p++ = (unsigned char)x;
-	}
-	return (s);
-}
-
 #include <stdio.h>
-int main()
-{
-	char s[] = "Hello my name is Bello.";
-	ft_memset((s + 17), 'X', 5);
-	printf("%s\n", s);
-}
 
+void *ft_calloc(size_t nmemb, size_t size)
+{
+	void *ptr;
+
+	ptr = (void *)malloc (nmemb * size);
+	if (array == NULL)
+		return (NULL);
+	ft_bzero(array, nmemb * size);
+	return (array);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wshee <wshee@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 10:28:46 by wshee             #+#    #+#             */
-/*   Updated: 2024/11/09 17:59:50 by wshee            ###   ########.fr       */
+/*   Updated: 2024/11/09 20:55:18 by wshee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,6 @@ void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
 char *ft_strnstr(const char *big, const char *little, size_t len);
 int ft_atoi(const char *nptr);
+void *calloc(size_t nmemb, size_t size);
 
 #endif //LIBFT_H
